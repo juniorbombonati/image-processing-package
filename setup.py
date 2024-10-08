586c312d8ac243143817954df6b99e9274e39fbd
juniorbombonati <juniorbombonati@yahoo.com>
2024-10-08 19:54:14 -0300

    Alteraçoes no arquivo setup.py

--- setup.py
+++ setup.py
@@ -10,10 +10,12 @@ setup(
     name = 'image_processing_josebombonati',
     version = '0.0.1',
     author = 'jose bombonati',
-    description = page_description,
-    long_description_content_type = 'text/markdown', 
+    description = 'Com o pacote image-processing, você poderá realizar transformações incríveis em suas imagens',
+    long_description = page_description,
+    long_description_content_type = 'text/markdown',
     url =  'https://github.com/juniorbombonati/image-processing-package',
     packages = find_packages(),
     install_requires = requirements,
     python_requires = '>=3.5'
 )
+
